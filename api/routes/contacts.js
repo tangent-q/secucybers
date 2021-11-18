fdaef74b6f5f654cd47412bb9e27fbf22855744c
TangentQ <admin@tangentq.com>
2021-11-18 05:08:02 +0000

    improve api response

--- api/routes/contacts.js
+++ api/routes/contacts.js
@@ -4,7 +4,7 @@ const service = require('./../services/contacts')
 
 router.post('/get-in-touch/', async function (req, res, next) {
   await service.getInTouch(req.body)
-  res.send({ success: 'Your form is submitted' })
+  res.send({})
 })
 
 module.exports = router
