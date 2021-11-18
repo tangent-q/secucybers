4428cdff8110df974828ee62761d72c25beaa875
TangentQ <admin@tangentq.com>
2021-11-18 05:14:23 +0000

    handle multipart/form-data

--- api/routes/contacts.js
+++ api/routes/contacts.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const multer = require('multer')
+const upload = multer()
 const service = require('./../services/contacts')
 
-router.post('/get-in-touch/', async function (req, res, next) {
+router.post('/get-in-touch/', upload.none(), async function (req, res, next) {
   await service.getInTouch(req.body)
   res.send({})
 })
